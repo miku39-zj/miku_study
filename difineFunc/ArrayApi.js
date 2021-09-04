fdbb789d35ebb6dc3fd60714fa604b22aabf753d
miku39 <2623343730@qq.com>
2021-09-04 08:32:39 +0800

    1

--- difineFunc/ArrayApi.js
+++ difineFunc/ArrayApi.js
@@ -1,10 +1,5 @@
-/*
- * 2021-08-12 18:54:40
- * @create by: zj
- * @Description: 
- */
-
 // 手写 filter
+Object.prototype.hasOwnProperty
 Array.prototype.myFilter = function (cb, thisArg) {
   if (this === undefined) {
     return new TypeError("undefined")
@@ -18,13 +13,13 @@ Array.prototype.myFilter = function (cb, thisArg) {
   for (let i = 0; i < len; i++) {
     if (i in arr) {
       if (cb.call(thisArg, arr[i], i, arr)) {
-        res.push (arr[i])
+        res.push(arr[i])
       }
     }
   }
   return res
 }
-console.log([1, 2, 3].myFilter(x=> x > 2));
+console.log([1, 2, 3].myFilter(x => x > 2));
 
 //手写 map
 Array.prototype.myMap = function (cb, thisArg) {
@@ -57,18 +52,16 @@ Array.prototype.myreduce = function (cb, initVal) {
   const len = arr.length
   let k = 0
   if (res === undefined) {
-    if (accumulator === undefined) {
-      while (k < len && !(k in arr)) {
-        k++;
-      }
-      // 如果超出数组界限还没有找到累加器的初始值，则TypeError
-      if (k >= len) {
-        throw new TypeError('Reduce of empty array with no initial value');
-      }
-      res = arr[k++];
+    while (k < len && !(k in arr)) {
+      k++;
     }
+    // 如果超出数组界限还没有找到累加器的初始值，则TypeError
+    if (k >= len) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    }
+    res = arr[k++];
   }
-  while(k < len) {
+  while (k < len) {
     if (k in arr) {
       res = cb.call(null, res, arr[k], k, arr)
     }
--- difineFunc/assign.js
+++ difineFunc/assign.js
@@ -3,6 +3,7 @@
  * @create by: zj
  * @Description: 
  */
+// Object.assign()
 Object.myAssign =  function(target, ...source) {
   if (target === null || target === undefined) {
     throw new TypeError("")
@@ -12,6 +13,23 @@ Object.myAssign =  function(target, ...source) {
     if(obj !== null) {
       Object.keys(obj).forEach(key => {
         res[key] = obj[key]
+      })
+    }
+  })
+  return res
+}
+// Object.prototype.propertyIsEnumerable
+Object.assign =  function(target, ...source) {
+  if (target === null || target === undefined) {
+    throw new TypeError("")
+  }
+  let res = Object(target)
+  source.forEach(obj => {
+    if(obj !== null) {
+      Reflect.ownKeys(obj).forEach(key => {
+        if(obj.propertyIsEnumerable(key)) {
+          res[key] = obj[key]
+        }
       })
     }
   })
